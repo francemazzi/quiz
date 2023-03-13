0e8d7b34c3bc473c26d1fb980abf11d977ac090a
FraSma <francemazzi@gmail.com>
2023-03-13 21:11:05 +0100

    fix: button cycle

--- lib/widget/ask.dart
+++ lib/widget/ask.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 import '../question_list.dart';
+import 'button_answer.dart';
 
 class Ask extends StatelessWidget {
   final Function clickButton;
@@ -25,48 +26,36 @@ class Ask extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        ElevatedButton(
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-              const EdgeInsets.all(15.0), // Qui definiamo il padding
-            ),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                    100.0), // Qui definiamo il raggio di curvatura
-              ),
-            ),
-            textStyle: MaterialStateProperty.all<TextStyle>(
-              const TextStyle(
-                fontFamily: 'Inter',
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.w500,
-                fontSize: 17.0,
-                height: 28.0 / 17.0,
-              ),
-            ),
-            backgroundColor:
-                MaterialStateProperty.all<Color>(const Color(0xFF5319FF)),
-          ),
-          onPressed: () {
-            clickButton(0);
-          },
-          child: Text(questions[0].answer[0].toString()),
-        ),
-        ElevatedButton(
-          onPressed: () => {clickButton(1)},
-          child: Text(questions[0].answer[1].toString()),
-        ),
-        ElevatedButton(
-          onPressed: () => {clickButton(2)},
-          child: Text(questions[0].answer[2].toString()),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            clickButton(3);
-          },
-          child: Text(questions[0].answer[3].toString()),
-        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < questions[0].answer.length; i++)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AskButton(clickButton, questions[0].answer[i].toString()),
+                  const SizedBox(height: 10),
+                ],
+              )
+          ],
+        )
+
+        // AskButton(clickButton, questions[0].answer[0].toString()),
+        // ElevatedButton(
+        //   onPressed: () => {clickButton(1)},
+        //   child: Text(questions[0].answer[1].toString()),
+        // ),
+        // ElevatedButton(
+        //   onPressed: () => {clickButton(2)},
+        //   child: Text(questions[0].answer[2].toString()),
+        // ),
+        // ElevatedButton(
+        //   onPressed: () {
+        //     clickButton(3);
+        //   },
+        //   child: Text(questions[0].answer[3].toString()),
+        // ),
       ],
     );
   }
---
+++ lib/widget/button_answer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../question_list.dart';
+
+class AskButton extends StatelessWidget {
+  final Function clickButton;
+  final String textAnswer;
+  const AskButton(this.clickButton, this.textAnswer, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //onPressed:  disabled ?  null : clickButton
+    return ElevatedButton(
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          const EdgeInsets.all(15.0), // Qui definiamo il padding
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+                100.0), // Qui definiamo il raggio di curvatura
+          ),
+        ),
+        textStyle: MaterialStateProperty.all<TextStyle>(
+          const TextStyle(
+            fontFamily: 'Inter',
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.w500,
+            fontSize: 17.0,
+            height: 28.0 / 17.0,
+          ),
+        ),
+        backgroundColor:
+            MaterialStateProperty.all<Color>(const Color(0xFF5319FF)),
+      ),
+      onPressed: () {
+        clickButton(0);
+      },
+      child: Text(textAnswer),
+    );
+  }
+}
